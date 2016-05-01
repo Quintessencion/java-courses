5acb4b7dd81d10428cbdc315674d216597dd861f
Kvint <zzerrorzz@list.ru>
2016-05-01 14:41:16 +0300

    user input

--- Calculate.java
+++ Calculate.java
@@ -1,6 +1,10 @@
 public class Calculate{
 	
-	public static void main(String[] args){
+	public static void main(String[] arg){
 		System.out.println("Calculate...");
+		int first = Integer.parseInt(arg[0]);
+		int second = Integer.parseInt(arg[1]);
+		int sum = first + second;
+		System.out.println("Sum " + sum);
     }
 }
